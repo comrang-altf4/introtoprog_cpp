ec0de1dd550921bfe5a80e6da672e240fc521fcb
comrang-altf4 <53970293+comrang-altf4@users.noreply.github.com>
2021-03-11 22:01:38 +0700

    chapter 10-15

---
+++ test_code/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+using namespace std;
+void pointers()
+{
+    cout<<"CHAPTER 11: POINTERS"<<endl;
+    char c = 'a';
+    char* p = &c;
+    cout << "The value of the dereferenced pointer is: " << *p<<endl;
+    int x = 123;
+    int* p1 = &x;
+    cout << "The value of the dereferenced pointer is: " << *p<<endl;
+    int* p2 = &x;
+    *p = 456; // change the value of pointed-to object
+    cout << "The value of x is: " << x;
+}
+void references()
+{
+    cout<<"CHAPTER 12: REFERENCES"<<endl;
+    int x = 123;
+    int &y = x; //y is an alias for x
+    cout<<"x: "<<x<<' '<<endl;
+    x = 456;
+    cout<<"y: "<<y<<' '<<endl;
+    y = 789;
+    cout<<"x: "<<x<<' '<<endl;
+
+    int a = 123;
+    const int& b = a; // const reference
+    a = 456;
+    cout<<"b: "<<b<<endl;
+}
+void strings()
+{
+    cout<<"CHAPTER 13: STRINGS"<<endl;
+    string s = "Hello world";
+    cout<<s<<endl;
+    char c='!';
+    s+=c;
+    cout<<s<<endl;
+    cout<<s[0]<<' '<<s.at(0)<<endl;
+    cout<<s.c_str()<<endl;
+
+    string s1="Hello",s2="World";
+    if (s1==s2)
+        cout<<"The strings are equal"<<endl;
+    else
+        cout<<"The strings are not equal"<<endl;
+
+    string s3;
+    cout<<"Input text: ";
+    cin>>s3;
+    cout<<"Your text: "<<s3<<endl;
+
+    string s4="Hello world";
+    string s5=s4.substr(3,4);
+    cout<<s4<<"||"<<s5<<endl;
+
+    string s6="Hello world";
+    string::size_type found=s.find("world");
+    if (found!=string::npos)
+        cout<<"Substring found at position: " << found;
+    else
+        cout << "The substring is not found.";
+}
+void exercise_chapter15()
+{
+    cout<<"CHAPTER 15: EXERCISES"<<endl;
+//Ex 1:
+    double a[5]= {1.1,2.2,3.3,4.4,5.5};
+    a[0]+=1;
+    a[4]+=1;
+    cout<<"1st element: "<<a[0]<<endl;
+    cout<<"Last element: "<<a[4]<<endl;
+    cout<<endl;
+//Ex 2:
+    double d=1.1;
+    double *p=&d;
+    cout<<"The value of the pointed-to object is: " << *p<<endl;
+//Ex 3:
+    double d2=1.1;
+    double &ref_d=d2;
+    d2+=1;
+    cout << "The values are: " << d2 << " and " << ref_d<<endl;
+    ref_d+=1;
+    cout << "The values are: " << d2 << " and " << ref_d<<endl;
+//Ex 4:
+    string s1="Res",s2="ult";
+    string s3=s1+s2;
+    cout<< "The resulting string is: "<<s3<<endl;
+//Ex 5:
+    string fullname;
+    cout<<"Please enter the first and the last name: ";
+    getline(cin,fullname);
+    cout<<"Your name is: "<<fullname<<endl;
+//Ex 6:
+    fullname="John Doe";
+    string firstname=fullname.substr(0,4),lastname=fullname.substr(5,3);
+    cout<<"Fullname is: "<<fullname<<endl;
+    cout<<"Firstname is: "<<firstname<<endl;
+    cout<<"Lastname is: "<<lastname<<endl;
+//Ex 7:
+    string s="Hello C++ World";
+    auto found=s.find('C');
+    if (found!=string::npos)
+        cout<<"Character found at position: "<<found<<endl;
+    else
+        cout<< "Character was not found."<<endl;
+//Ex 8:
+    found=s.find("C++");
+    if (found!=string::npos)
+        cout<<"Character found at position: "<<found<<endl;
+    else
+        cout<< "Character was not found."<<endl;
+//Ex 9:
+    auto c = 'a';
+    auto x = 123;
+    auto d1 = 3.14;
+    std::cout << "The type of c is deduced as char, the value is: "
+              << c <<endl;
+    std::cout << "The type of x is deduced as int, the value is: "
+              << x <<endl;
+    std::cout << "The type of d is deduced as double, the value is: "
+              << d1 <<endl;
+}
+int main()
+{
+    pointers();
+    references();
+    strings();
+    exercise_chapter15();
+    return 0;
+}
